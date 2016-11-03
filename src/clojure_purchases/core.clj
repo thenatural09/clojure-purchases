6d2d1d20842edc0150fd9c98a0df10f43ec0f711
Troy <tward4@tulane.edu>
2016-11-02 21:25:35 -0400

    Added filtering

--- src/clojure_purchases/core.clj
+++ src/clojure_purchases/core.clj
@@ -1,6 +1,27 @@
 (ns clojure-purchases.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
+(def file-name "purchases.csv")
+
 (defn -main [& args]
-  (println "Hello, world!"))
+  (let [purchases (str/split-lines(slurp file-name))        
+        purchases (map (fn [line]                         
+                         (str/split line #","))                    
+                    purchases)        
+        header (first purchases)        
+        purchases (rest purchases)        
+        purchases (map (fn [line]                         
+                         (zipmap header line))                    
+                    purchases)        
+        purchases (filter (fn [line]                            
+                            (= (get line "category")                               
+                               "Alcohol"))                    
+                    purchases)        
+        purchases (pr-str purchases)]    
+    (spit "purchases.edn" purchases)))
+    
+                         
+  
+  
 
